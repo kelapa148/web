a7fafe9e5e113f08fe1f9ecfa8cc2ba042ffb20e
kelapa148 <surat@coconut.or.id>
2022-08-22 23:15:36 +0700

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -31,35 +31,35 @@ const ContactPage = ({ data: { site } }) => {
           <h1 className="post-title">Contact us</h1>
           <div className="primary-content">
             If you are interested to join the lab or collaborate, please do not
-            hesitate to send Titipat an email (my.titipat [AT] gmail.com) or
+            hesitate to send Titipat an email (surat [AT] coconut.or.id) or
             using the form in this page &rarr;
           </div>
           <div className="primary-content">
             <b>Undergraduate students: </b>
-            Please contact Titipat to arrange a short meeting to discuss about
+            Please contact Obo to arrange a short meeting to discuss about
             research and/or potential projects.
           </div>
           <div className="primary-content">
             <b>Graduate students: </b>
-            Due to limitation of the Mahidol university's rule, I cannot hire
+            Due to limitation of the Mahidol university's rule, We cannot hire
             graduate students yet. If you are interested in doing research,
             please send a CV with a brief research interest. Then you can
             arrange a discussion before applying. You can potentially apply to
-            the Department of Biomedical engineering at Mahidol University,
-            Vidyasirimedhi Institute of Science and Technology (VISTEC), or
-            Chulalongkorn University, Thailand, where I can co-advise. There are
+            the Department of Information System at STMIK Profesional Makassar,
+            Dipanegara University (UNDIPA), or
+            Muhammadiyah University Of Makassar, Indonesia, where I can co-advise. There are
             multiple options if you are interested.
           </div>
           <div className="primary-content">
             <b>Other positions: </b>I always actively look for interns and
             developers. If you are interested in doing internship (locally or
             remotely) or other positions such as developers, please also do not
-            hesitate to email Titipat to discuss.
+            hesitate to email us to discuss.
           </div>
           <div className="primary-content">
             <b>Office: </b>
-            Mahidol University, Engineering Building 3, 3rd Floor 999
-            Phutthamonthon 4 Road, Salaya, Nakhon Pathom, Thailand 73170
+            Jl. Mon. Emmy Saelan III, 2rd Floor, Makassar City, 
+            Sulawesi Selatan, Indonesia 90221.
           </div>
         </div>
         <div>
