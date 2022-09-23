2a1e0714a663c1327665eb7352837ad1b751fceb
kelapa148 <surat@coconut.or.id>
2022-09-23 23:37:42 +0800

    Update people.js

--- src/pages/people.js
+++ src/pages/people.js
@@ -42,11 +42,11 @@ const PeoplePage = ({
       <div className="grids small" style={{ marginBottom: "32px" }}>
         {currentPeople}
       </div>
-      <h2>Previous lab members</h2>
+      <h2>Active Members</h2>
       <div className="grids small" style={{ marginBottom: "32px" }}>
         {activePeople}
       </div>
-      <h2>Active Members</h2>
+      <h2>Previous lab members</h2>
       <div className="grids small" style={{ marginBottom: "32px" }}>
         {prevPeople}
       </div>
