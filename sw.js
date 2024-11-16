742fe3991dd4c8fa0b1b1e218e8b852122a61857
kelapa148 <surat@coconut.or.id>
2024-11-16 21:26:43 +0000

    Deploying to gh-pages from @ kelapa148/web@2c07dd0abd92154056fa2e265fa07ebc4a53b98c 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fe86dce4f76836ff720e.js"
+    "url": "webpack-runtime-f3ec12617f274f00df2c.js"
   },
   {
     "url": "framework-31413af41550353e296a.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3e2b717420f13c0532ad0c97b9789dc3"
+    "revision": "f04dc52e2a5cac0044a9565dd1ceaee5"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
