be0b0d1fad959b0f8f2ede7b7d04ab1b6f1d488b
oghie <oghie.yesterday@gmail.com>
2026-09-20 15:22:20 +0000

    Deploying to gh-pages from @ kelapa148/web@4aa729bed1dfd4e190aec191ffeb087f07419305 🚀

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.__precacheManifest = [
     "url": "framework-31b40719cb76dbe7926d.js"
   },
   {
-    "url": "styles.0943b0b2e52f54237196.css"
+    "url": "styles.123debc8fd2c8059d46f.css"
   },
   {
     "url": "styles-f2c1808a00cfb3adf39e.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5296f559b8f4187d634c990a7ff7e835"
+    "revision": "5c3f5873daa0f6d0b122b9d56cb5c157"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
