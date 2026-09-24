eb37e9ec52641b3bcb4d54b5712b2f960e3884ff
oghie <oghie.yesterday@gmail.com>
2026-09-24 16:49:08 +0000

    Deploying to gh-pages from @ kelapa148/web@5ea4666368b10d9e99ed2641053366b29cd46bb6 🚀

--- sw.js
+++ sw.js
@@ -27,32 +27,32 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e2b5ca9e3796386b7650.js"
+    "url": "webpack-runtime-66aaba3f766a5ea77b44.js"
   },
   {
-    "url": "framework-e245a832c7032f68d3af.js"
+    "url": "framework-aec39f55470b3921e272.js"
   },
   {
-    "url": "styles.2c069412399b6f99131b.css"
+    "url": "styles.015ca9ebe5b38770be47.css"
   },
   {
-    "url": "styles-719967e2ea75025a1d91.js"
+    "url": "styles-77e32ee8d6888701d03f.js"
   },
   {
     "url": "532a2f07-cee85b289ce281e20d67.js"
   },
   {
-    "url": "app-1907f524faecfce5318e.js"
+    "url": "app-29ced8d4887605652238.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7a6d67b6fedc882e32a83845eef59b0d"
+    "revision": "6be21915826d7b769cc01f1025dfc400"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
   },
   {
-    "url": "polyfill-d2e1ad84fb1493fcbe1a.js"
+    "url": "polyfill-2f6e4c7f24f44b07bfb4.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -148,7 +148,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-1907f524faecfce5318e.js`))) {
+  if (!resources || !(await caches.match(`/app-29ced8d4887605652238.js`))) {
     return await fetch(event.request)
   }
 
